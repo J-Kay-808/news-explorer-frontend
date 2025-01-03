f7122d01fb8bc9f4455adaaaba2c7d6e536bfde7
= <=>
2025-01-03 13:46:55 -0600

    modified

--- src/utils/news-api.js
+++ src/utils/news-api.js
@@ -4,9 +4,14 @@
   const lastWeek = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString().split("T")[0];
   const BASE_URL =
   process.env.NODE_ENV === "production"
-  ? 'www.newsexplorer.chickenkiller.com'
-  : "https://newsapi.org/v2/everything";
+    ? "https://nomoreparties.co/news/v2/everything"
+    : "https://newsapi.org/v2/everything";
     
+  // const BASE_URL =
+  // process.env.NODE_ENV === "production"
+  // ? 'www.newsexplorer.chickenkiller.com'
+  // : "https://newsapi.org/v2/everything";
+
   // const BASE_URL = process.env.NODE_ENV === "production"
   //   ? 'www.newsexplorer.chickenkiller.com' :
   //   `https://newsapi.org/v2/everything?q=${keyword}&from=${lastWeek}&to=${today}&pageSize=100&apiKey=${apiKey}`;
