db9bb77e01b19e60b76b43bf49e9fb55d1fc96e0
= <=>
2025-01-03 13:51:43 -0600

    new base_url

--- src/utils/news-api.js
+++ src/utils/news-api.js
@@ -1,20 +1,20 @@
 
-  const apiKey = "b82fbe2089d54559acbd35ecbd77a698";
-  const today = new Date().toISOString().split("T")[0];
-  const lastWeek = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString().split("T")[0];
-  const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://nomoreparties.co/news/v2/everything"
-    : "https://newsapi.org/v2/everything";
-    
-  // const BASE_URL =
-  // process.env.NODE_ENV === "production"
-  // ? 'www.newsexplorer.chickenkiller.com'
-  // : "https://newsapi.org/v2/everything";
+const apiKey = "b82fbe2089d54559acbd35ecbd77a698";
+const today = new Date().toISOString().split("T")[0];
+const lastWeek = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString().split("T")[0];
+const BASE_URL = process.env.NODE_ENV === "production"
+  ? "https://nomoreparties.co/news/v2/everything"
+  : "https://newsapi.org/v2/everything";
 
-  // const BASE_URL = process.env.NODE_ENV === "production"
-  //   ? 'www.newsexplorer.chickenkiller.com' :
-  //   `https://newsapi.org/v2/everything?q=${keyword}&from=${lastWeek}&to=${today}&pageSize=100&apiKey=${apiKey}`;
+
+// const BASE_URL =
+// process.env.NODE_ENV === "production"
+// ? 'www.newsexplorer.chickenkiller.com'
+// : "https://newsapi.org/v2/everything";
+
+// const BASE_URL = process.env.NODE_ENV === "production"
+//   ? 'www.newsexplorer.chickenkiller.com' :
+//   `https://newsapi.org/v2/everything?q=${keyword}&from=${lastWeek}&to=${today}&pageSize=100&apiKey=${apiKey}`;
 
 // export function fetchNewsArticles(keyword) {
 //   return fetch(BASE_URL)
