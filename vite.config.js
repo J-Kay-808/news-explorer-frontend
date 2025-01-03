ceda8d5dfb1ad350bf838b9ec26b88da9d0ba308
= <=>
2025-01-03 12:48:09 -0600

    rendering new code

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/news-explorer-frontend/',
   plugins: [react()],
+  base: '/news-explorer-frontend/',
   server: {     
     port: 3000,
   },
